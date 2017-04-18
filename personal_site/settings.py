afd60b4d0bb3c6ba38afd7a99ee5eb54ac2d82c1
kokopelli314 <nacclonts@gmail.com>
2017-04-17 20:09:22 -0600

    setting adjusted

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = secret_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nathanclonts.com', 'https://nathanclonts.com', '45.55.66.186',]
 
 
 # Application definition
