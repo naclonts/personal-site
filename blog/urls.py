810e5f1987bf62ceb5d6a6f38047486f3a4a9fb0
kokopelli314 <nacclonts@gmail.com>
2017-04-22 12:06:21 -0600

    set up rudimentary post list view

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.post_list, name='post_list'),
+#    url(r'posts')
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
 def index(request):
     return render(request, 'blog/index.html')
 
+def post_list(request):
+    posts = models.Post.objects.all()
+    print(posts)
+    context = { 'posts': posts }
+    return render(request, 'blog/post_list.html', context=context)
