17b4721794ef1afc26df576f14792390fdb8f96a
kokopelli314 <nacclonts@gmail.com>
2017-04-22 19:00:18 -0600

    added post viewing pages, initial footer

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
+
+import string
+
+def get_url_title(title):
+    print(title)
+    punctuation = set(string.punctuation)
+    title_no_punc = ''.join(ch for ch in title if ch not in punctuation)
+    title_recombo = ''.join(title_no_punc).lower()
+    return '_'.join(title_recombo.split())
 
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
+    slug = models.SlugField(unique=True, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     
@@ -13,6 +24,93 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
         
+    def save(self, **kwargs):
+        slug_str = '%s' % self.title
+        unique_slugify(self, slug_str.lower())
+        super(Post, self).save(**kwargs)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return 'blog:posts', (self.slug,)
+    
+    def url_title(self):
+        punctuation = set(string.punctuation)
+        title_no_punc = ''.join(ch for ch in self.title if ch not in punctuation)
+        title_recombo = ''.join(title_no_punc).lower()
+        return '_'.join(title_recombo.split())
+
     def __str__(self):
         return self.title
 
+
+import re
+from django.template.defaultfilters import slugify
+
+def unique_slugify(instance, value, slug_field_name='slug', queryset=None,
+                   slug_separator='-'):
+    """
+    Calculates and stores a unique slug of ``value`` for an instance.
+
+    ``slug_field_name`` should be a string matching the name of the field to
+    store the slug in (and the field to check against for uniqueness).
+
+    ``queryset`` usually doesn't need to be explicitly provided - it'll default
+    to using the ``.all()`` queryset from the model's default manager.
+    """
+    slug_field = instance._meta.get_field(slug_field_name)
+
+    slug = getattr(instance, slug_field.attname)
+    slug_len = slug_field.max_length
+
+    # Sort out the initial slug, limiting its length if necessary.
+    slug = slugify(value)
+    if slug_len:
+        slug = slug[:slug_len]
+    slug = _slug_strip(slug, slug_separator)
+    original_slug = slug
+
+    # Create the queryset if one wasn't explicitly provided and exclude the
+    # current instance from the queryset.
+    if queryset is None:
+        queryset = instance.__class__._default_manager.all()
+    if instance.pk:
+        queryset = queryset.exclude(pk=instance.pk)
+
+    # Find a unique slug. If one matches, at '-2' to the end and try again
+    # (then '-3', etc).
+    next = 2
+    while not slug or queryset.filter(**{slug_field_name: slug}):
+        slug = original_slug
+        end = '%s%s' % (slug_separator, next)
+        if slug_len and len(slug) + len(end) > slug_len:
+            slug = slug[:slug_len-len(end)]
+            slug = _slug_strip(slug, slug_separator)
+        slug = '%s%s' % (slug, end)
+        next += 1
+
+    setattr(instance, slug_field.attname, slug)
+
+
+def _slug_strip(value, separator='-'):
+    """
+    Cleans up a slug by removing slug separator characters that occur at the
+    beginning or end of a slug.
+
+    If an alternate separator is used, it will also replace any instances of
+    the default '-' separator with the new separator.
+    """
+    separator = separator or ''
+    if separator == '-' or not separator:
+        re_sep = '-'
+    else:
+        re_sep = '(?:-|%s)' % re.escape(separator)
+    # Remove multiple instances and if an alternate separator is provided,
+    # replace the default '-' separator.
+    if separator != re_sep:
+        value = re.sub('%s+' % re_sep, separator, value)
+    # Remove separator from the beginning and end of the slug.
+    if separator:
+        if separator != '-':
+            re_sep = re.escape(separator)
+        value = re.sub(r'^%s+|%s+$' % (re_sep, re_sep), '', value)
+    return value
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
-from . import models
+from .models import Post
 
 def index(request):
     return render(request, 'blog/index.html')
 
 def post_list(request):
-    posts = models.Post.objects.all()
+    posts = Post.objects.all()
     print(posts)
     context = { 'posts': posts }
     return render(request, 'blog/post_list.html', context=context)
+
+def post_detail(request, post_url):
+    print('-------------------------------'+post_url)
+    post = get_object_or_404(Post, url_title=post_url)
+    return render(request, 'blog/post.html')
+
+
+class PostView(generic.DetailView):
+    model = Post
+#    return render(request, 'blog/post.html', context=context)
