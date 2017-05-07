a03d77b311bcb2a1e8a269b27b62a2f51ae24d7b
kokopelli314 <nacclonts@gmail.com>
2017-05-06 23:03:08 -0600

    added initial contact page and reorganized dirs

--- personal_site/urls.py
+++ personal_site/urls.py
@@ -18,9 +18,12 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
     url(r'^', include('homepage.urls', namespace='homepage')),
     url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'^projects/', include('projects.urls', namespace='projects')),
+    url(r'^contact', views.contact_info, name="contact"),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
