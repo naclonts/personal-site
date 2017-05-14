ae46f9b435120d2d56fea5e0a5145298383ff56a
kokopelli314 <nacclonts@gmail.com>
2017-05-13 18:19:26 -0600

    moved projects to seperate page

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.splash_page, name='splash_page'),
+    url(r'^$', views.splash_page, name='splash'),
     # url(r'^post/(?P<slug>[\w\-]+)$', views.PostView.as_view(), name='post'),
 ]
