bf51ebfc59cc97120e02aa02ccf9a13bb440c989
kokopelli314 <nacclonts@gmail.com>
2017-08-04 12:21:09 -0600

    implemented slide-in nav menu

--- static/js/base-interactions.js
+++ static/js/base-interactions.js
@@ -1,9 +1,15 @@
 
 function baseSetUp() {
+    // Handle burger menu
     $('.hamburger').click(function(e) {
         $(this).toggleClass('is-active');
+        $('.menu').toggleClass('hidden');
+    });
+
+    $('.menu-link').click(function(e) {
+        $('.hamburger').removeClass('is-active');
+        $('.menu').addClass('hidden');
     });
 }
-
 
 $(document).ready(baseSetUp);
