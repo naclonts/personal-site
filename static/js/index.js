5dbef370b99272512d718f79fb67622abb41b7de
Nathan Clonts <nacclonts@gmail.com>
2019-03-16 22:56:59 -0600

    bring in npm & webpack

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,5 @@
+import baffle from 'baffle'
+
 function baseSetUp() {
     // Handle burger menu
     $('.hamburger').click(function(e) {
@@ -9,6 +11,16 @@ function baseSetUp() {
         $('.hamburger').removeClass('is-active');
         $('.menu').addClass('hidden');
     });
+
+
+    // Set up baffle text
+    let b = baffle('.baffle-me');
+    b.set({
+        characters: '█▓█ ▒░/▒░ █░▒▓/ █▒▒ ▓▒▓/█ ░█▒/ ▒▓░ █<░▒ ▓/░>',
+    })
+    b.start();
+    b.reveal(5000);
 }
 
 $(document).ready(baseSetUp);
+
