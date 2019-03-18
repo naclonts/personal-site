6da3b1c0de9025972d8d6c9674bbfc6b992a3da4
Nathan Clonts <nacclonts@gmail.com>
2019-03-17 22:48:16 -0600

    remove awesome-button webcomponent

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,29 @@
 import baffle from 'baffle'
 
+(function ($) {
+    /**
+     * Copyright 2012, Digital Fusion
+     * Licensed under the MIT license.
+     * http://teamdf.com/jquery-plugins/license/
+     *
+     * @author Sam Sehnert
+     * @desc A small plugin that checks whether elements are within
+     *     the user visible viewport of a web browser.
+     *     only accounts for vertical position, not horizontal.
+     */
+    $.fn.visible = function (partial) {
+        var $t = $(this),
+            $w = $(window),
+            viewTop = $w.scrollTop(),
+            viewBottom = viewTop + $w.height(),
+            _top = $t.offset().top,
+            _bottom = _top + $t.height(),
+            compareTop = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+    };
+})(jQuery);
+
 function baseSetUp() {
     // Handle burger menu
     $('.hamburger').click(function(e) {
@@ -19,8 +43,42 @@ function baseSetUp() {
         characters: '<>01█▓█ ▒░/▒░ █░▒▓/ █▒▒ ▓▒▓/█ ░█▒/ ▒▓░ █<░▒ ▓/░>',
         exclude: ['D']
     })
-    b.start();
-    b.reveal(1000, 400);
+    let baffleDone = false;
+
+    // Trigger events when elements are scrolled into view...
+    $(window).scroll(function () {
+        var hT = $('#baffle-1').offset().top,
+            hH = $('#baffle-1').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+        if (wS > (hT + hH - wH)) {
+            if (baffleDone) {
+                return;
+            }
+            b.start();
+            b.reveal(1000, 400);
+            baffleDone = true;
+        }
+    });
+
+    var allMods = $(".slide-on-scroll");
+    allMods.each(function (i, el) {
+        var el = $(el);
+        el.addClass('slide-on-scroll--translate-class');
+        if (el.visible(true)) {
+            el.addClass("already-visible");
+        }
+    });
+
+    $(window).scroll(function (event) {
+        allMods.each(function (i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                el.addClass("come-in");
+            }
+        });
+
+    });
 }
 
 $(document).ready(baseSetUp);
